c19d48e0fe1df2c42158d7c5679bba93032fe93f
HasanliHilal <hasanlihilal@gmail.com>
2022-05-06 17:55:29 +0400

    Show_Movie_Data

--- app/src/main/java/com/example/mvvm_project/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/mvvm_project/presentation/movie/MovieActivity.kt
@@ -3,9 +3,12 @@ package com.example.mvvm_project.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvm_project.R
 import com.example.mvvm_project.databinding.ActivityMovieBinding
 import com.example.mvvm_project.presentation.di.Injector
@@ -15,19 +18,40 @@ class MovieActivity : AppCompatActivity() {
     @Inject
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieViewModel: MovieViewModel
-    private lateinit var binding:ActivityMovieBinding
+    private lateinit var binding: ActivityMovieBinding
+    private lateinit var adapter: MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_movie)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
         (application as Injector).createMovieSubComponent()
             .inject(this)
 
-        movieViewModel=ViewModelProvider(this,factory)
+        movieViewModel = ViewModelProvider(this, factory)
             .get(MovieViewModel::class.java)
+        initRecylerView()
+
+    }
+
+    private fun initRecylerView() {
+        binding.recylistMovie.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.recylistMovie.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies() {
+        binding.prgBarMovie.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("MAYTAG",it.toString())
-        })
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.prgBarMovie.visibility = View.GONE
 
+            } else {
+                binding.prgBarMovie.visibility = View.GONE
+                Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
